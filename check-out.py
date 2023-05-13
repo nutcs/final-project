6323437f473cd20081f484b170f262a4c2090100
nutcs <126545516+nutcs@users.noreply.github.com>
2023-05-13 22:15:21 +0700

    Update check-out.py

--- check-out.py
+++ check-out.py
@@ -26,35 +26,37 @@ def check_outNut():
     FrameLogoBu.columnconfigure(0,weight=1)
 
 
-   
-    #img
-    Label(FrameLogoBu,image=logo,bg='#0194f3').grid(row=0,sticky=W)
-    Label(FrameLogoBu,text='BURangsit',bg='#0194f3',font="Calibri 32 bold").grid(row=0,sticky=E,padx=25)
-
     # FrameLogoBu_detail = Frame(FrameLogoBu)
     # FrameLogoBu_detail.grid(row=0,columnspan=2)
     # FrameLogoBu_detail.rowconfigure(0,weight=1)
     # FrameLogoBu_detail.columnconfigure(0,weight=1)
+    #img
+    Label(FrameLogoBu,image=logo,bg='#0194f3').grid(row=0,sticky=W)
+    Label(FrameLogoBu,text='BURangsit',bg='#0194f3',font="Calibri 32 bold").grid(row=0,sticky=E,padx=25)
+
+    padX_left = 200
+    padX_right = 200
     #infomation 
-    Label(mainFrame_checkout,text='Thaks Annซ๋า').grid(row=1,column=0,sticky=W,padx=25)
-    Label(mainFrame_checkout,text='Your booking in kiev is confirmed',font="Calibri 20 bold").grid(row=2,column=0,sticky=W,padx=25)
-    Label(mainFrame_checkout,text="Gar'is Kyiv Factory Hostel",font="Calibri 20 bold",fg='blue').grid(row=3,column=0,sticky=W,padx=25)
-    Label(mainFrame_checkout,image=img_test).grid(row=4,columnspan=2,pady=20)
+    Label(mainFrame_checkout,text='Thaks Annซ๋า').grid(row=1,columnspan=2,sticky=SW,padx=padX_left,pady=10)
+    Label(mainFrame_checkout,text='Your booking in kiev is confirmed',font="Calibri 20 bold").grid(row=2,columnspan=2,sticky=W,padx=padX_left)
+    Label(mainFrame_checkout,text="Gar'is Kyiv Factory Hostel",font="Calibri 20 bold",fg='blue').grid(row=3,columnspan=2,sticky=W,padx=padX_left)
+    #img
+    Label(mainFrame_checkout,image=img_test).grid(row=4,columnspan=2,pady=20,sticky=W,padx=padX_left)
 
     #detel
-    Label(mainFrame_checkout,text='Your reservation').grid(row=5,column=0,sticky=W,padx=25)
-    Label(mainFrame_checkout,text=f'{1} night, {1} dormitory bed').grid(row=5,column=1,sticky=E,padx=25)
+    Label(mainFrame_checkout,text='Your reservation').grid(row=5,columnspan=2,sticky=W,padx=padX_left)
+    Label(mainFrame_checkout,text=f'{1} night, {1} dormitory bed').grid(row=5,column=1,sticky=E,padx=padX_right)
     
-    Label(mainFrame_checkout,text='check-in').grid(row=6,column=0,sticky=W,padx=25)
-    Label(mainFrame_checkout,text=f'13/5/23').grid(row=6,column=1,sticky=E,padx=25)
+    Label(mainFrame_checkout,text='check-in').grid(row=6,columnspan=2,sticky=W,padx=padX_left)
+    Label(mainFrame_checkout,text=f'13/5/23').grid(row=6,columnspan=2,sticky=E,padx=padX_right)
     
-    Label(mainFrame_checkout,text='Check out').grid(row=7,column=0,sticky=W,padx=25)
-    Label(mainFrame_checkout,text='14/5/23').grid(row=7,column=1,sticky=E,padx=25)
+    Label(mainFrame_checkout,text='Check out').grid(row=7,columnspan=2,sticky=W,padx=padX_left)
+    Label(mainFrame_checkout,text='14/5/23').grid(row=7,columnspan=2,sticky=E,padx=padX_right)
     
-    Label(mainFrame_checkout,text='Prepayment').grid(row=8,column=0,sticky=W,padx=25)
-    Label(mainFrame_checkout,text='You will charged a prepayment of the total price at any time').grid(row=8,column=1,sticky=E,padx=25)
+    Label(mainFrame_checkout,text='Prepayment').grid(row=8,columnspan=2,sticky=W,padx=padX_left)
+    Label(mainFrame_checkout,text='You will charged a prepayment of the total price at any time').grid(row=8,columnspan=2,sticky=E,padx=padX_right)
 
-    Button(mainFrame_checkout,text='Cancel your booking',bg='orange',fg='white').grid(row=9,columnspan=2)
+    Button(mainFrame_checkout,text='Cancel your booking',bg='orange',fg='white').grid(row=9,columnspan=2,pady=20,sticky=S)
 
     Label(mainFrame_checkout,text='').grid(row=10)
     Label(mainFrame_checkout,text='').grid(row=11)
@@ -79,4 +81,3 @@ FrameLogoBu = Frame(mainFrame_checkout,bg='#0194f3')
 check_outNut()
 
 root.mainloop()
-
