2e5f055371bc0dbb3b6718bf5d35ec9ffdddf99e
nutcs <126545516+nutcs@users.noreply.github.com>
2023-05-14 12:14:22 +0700

    Update comment_review.py

--- comment_review.py
+++ comment_review.py
@@ -20,10 +20,11 @@ def treeview0() :
     comment_mainFrame.rowconfigure((1,2,3,4,5),weight=1)
     comment_mainFrame.columnconfigure((0,1,2),weight=1)
 
+    Label(comment_mainFrame,text="เลือกรีวิวห้องพักของคุณ",bg=bg_comment,font="Calibri 24 bold",fg=fg_comment).grid(row=0,columnspan=3,pady=20,padx=20,sticky=N)
     ################ Treeview
     global mytree,comment_entry,name_comment
     mytree= ttk.Treeview(comment_mainFrame,columns= ("col1","col2","col3") )
-    mytree.grid(row=0,columnspan=3,pady=20,padx=20)
+    mytree.grid(row=0,columnspan=3,pady=20,padx=20,sticky=S)
     mytree.heading("#0",text="Name")
     mytree.heading("col1",text="Username")
     mytree.heading("col2",text="Check-in")
@@ -44,15 +45,24 @@ def treeview0() :
     ################
 
     #name
-    name_comment = Label(comment_mainFrame,bg='white',font="Calibri 20 bold",text='none')#,textvariable=ntbasic
+    name_comment = Label(comment_mainFrame,bg=bg_comment,font="Calibri 20 bold",fg=fg_comment,text='none')#,textvariable=ntbasic
     name_comment.grid(row=1,columnspan=3)
-    #input entry
-    comment_entry = Entry(comment_mainFrame,width=70, borderwidth=2, relief="groove",font="Calibri 16")
-    comment_entry.grid(row=2,columnspan=3)
 
-    Button(comment_mainFrame,width=20,text='add comment',command=insertMyTree).grid(row=3,column=0,sticky=E)
-    Button(comment_mainFrame,width=20,text='Edit').grid(row=3,column=1)
-    Button(comment_mainFrame,width=20,text='Delete').grid(row=3,column=2,sticky=W)
+    #ให้คะแนน
+    Label(comment_mainFrame,text="ให้คะแนนที่พักของคุณ: ",bg=bg_comment,fg=fg_comment).grid(row=2,column=0,pady=20,padx=20,sticky=E)
+    star_comment = ttk.Combobox(comment_mainFrame,values=['1','2','3','4','5'],width=5)
+    star_comment.set('1')
+    star_comment.grid(row=2,column=1,sticky=W)
+    Label(comment_mainFrame,text="ดาว",bg=bg_comment,fg=fg_comment).grid(row=2,column=1,columnspan=2,sticky=W,padx=100)
+
+    #input entry
+    Label(comment_mainFrame,text="รีวิวห้องพัก: ",bg=bg_comment,fg=fg_comment).grid(row=3,column=0,sticky=E,padx=10)
+    comment_entry = Entry(comment_mainFrame,width=50, borderwidth=2, relief="groove",font="Calibri 16")
+    comment_entry.grid(row=3,columnspan=2,column=1,sticky=W)
+
+    Button(comment_mainFrame,width=20,text='add comment').grid(row=4,column=0,sticky=E)
+    Button(comment_mainFrame,width=20,text='Edit').grid(row=4,column=1)
+    Button(comment_mainFrame,width=20,text='Delete').grid(row=4,column=2,sticky=W)
     mytree.bind('<Double-1>',treeviewclick)
 
 def treeviewclick(e) :
@@ -66,61 +76,60 @@ def treeviewclick(e) :
     name_comment['text'] = clickdata[0]
 
 
-def addCourse():
-    if(comment_entry.get() == ''):
-        messagebox.showinfo("Admin","กรุณากรอก course_code")
-    else: 
-        print(number_course)
-        sql = '''
-        INSERT INTO course (id, course_code, course_name, day, room)
-        VALUES (?,?,?,?,?);
-        '''
-        cursor.execute(sql, [number_course + 1, enAdd.get(),enUpdate.get(),enDelete.get(),enClear.get()] )
-        conn.commit()
-        mytree.insert('','end',values=[number_course + 1, enAdd.get(),enUpdate.get(),enDelete.get(),enClear.get()])
+# def addCourse():
+#     if(comment_entry.get() == ''):
+#         messagebox.showinfo("Admin","กรุณากรอก comment")
+#     else: 
+#         print(number_course)
+#         sql = '''
+#         INSERT INTO course (id, course_code, course_name, day, room)
+#         VALUES (?,?,?,?,?);
+#         '''
+#         cursor.execute(sql, [number_course + 1, enAdd.get(),enUpdate.get(),enDelete.get(),enClear.get()] )
+#         conn.commit()
+#         mytree.insert('','end',values=[number_course + 1, enAdd.get(),enUpdate.get(),enDelete.get(),enClear.get()])
 
 
-def insertMyTree():
-    global number_course
-    sql = 'select * from course'
-    # cursor.execute(sql)
-    # course_data = cursor.fetchall()
-    number_course = len(course_data)
-    for i,record in enumerate(course_data):
-        mytree.insert('','end',values=record)
+
     
-def updateCourse():
-    if(enAdd.get() == ''):
-        messagebox.showinfo("Admin","กรุณากรอก course_code")
-    elif(enUpdate.get() == ''):
-        messagebox.showinfo("Admin","กรุณากรอก course_name")
-    elif(enDelete.get() == ''):
-        messagebox.showinfo("Admin","กรุณากรอก day")
-    elif(enClear.get() == ''):
-        messagebox.showinfo("Admin","กรุณากรอก room")
-    else: 
-        sql = 'update course set course_code=?, course_name=?, day=?, room=? where id=?'
-        cursor.execute(sql,[enAdd.get(),enUpdate.get(),enDelete.get(),enClear.get(), clickdata[0]])
-        conn.commit()
-        # delete mytree
-        for item in mytree.get_children():
-            mytree.delete(item)
-        insertMyTree()
+# def updateCourse():
+#     if(comment_entry.get() == ''):
+#         messagebox.showinfo("Admin","กรุณากรอก comment")
+#     else: 
+#         sql = 'update course set course_code=?, course_name=?, day=?, room=? where id=?'
+#         cursor.execute(sql,[comment_entry.get(),enUpdate.get(),enDelete.get(),enClear.get(), clickdata[0]])
+#         conn.commit()
+#         # delete mytree
+#         for item in mytree.get_children():
+#             mytree.delete(item)
+#         insertMyTree()
 
-def deleteCourse():
-    sql = 'delete from course where id=?;'
-    cursor.execute(sql,[clickdata[0]])
-    conn.commit()
-    for item in mytree.get_children():
-        mytree.delete(item)
-    insertMyTree()
+# def deleteCourse():
+#     sql = 'delete from course where id=?;'
+#     cursor.execute(sql,[clickdata[0]])
+#     conn.commit()
+#     for item in mytree.get_children():
+#         mytree.delete(item)
+#     insertMyTree()
+
+# def insertMyTree():
+#     global number_course
+#     sql = 'select * from course'
+#     cursor.execute(sql)
+#     course_data = cursor.fetchall()
+#     number_course = len(course_data)
+#     for i,record in enumerate(course_data):
+#         mytree.insert('','end',values=record)
+
 
 
 root = mainwindow()
 
-comment_mainFrame = Frame(root,bg='white')
+comment_mainFrame = Frame(root,bg='#0194f3')
 
 ntbasic = [StringVar()]
+bg_comment = '#0194f3'
+fg_comment = 'white'
 
 treeview0()
 root.mainloop()
